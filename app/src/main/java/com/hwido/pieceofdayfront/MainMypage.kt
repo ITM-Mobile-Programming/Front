eeec18a21282b5d7e66fd160acc968dd91bd846e
hwido <hyudojung@naver.com>
2023-11-29 08:22:09 +0900

    MainMypage part

--- app/src/main/java/com/hwido/pieceofdayfront/MainMypage.kt
+++ app/src/main/java/com/hwido/pieceofdayfront/MainMypage.kt
@@ -4,12 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.hwido.pieceofdayfront.databinding.MainMypageBinding
 
 class MainMypage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_mypage)
-	val test = "test"
+        val binding = MainMypageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 버튼 클릭 통한 페이지 변경
+        binding.mainMypageSharepageBtn.setOnClickListener {
+            val intent = Intent(this, MainMypageSharelist::class.java)
+            startActivity(intent)
+        }
+
+        binding.mainMypageMbtipageBtn.setOnClickListener {
+            val intent = Intent(this, MainMypageMBTI::class.java)
+            startActivity(intent)
+        }
 
         // 하단 버튼 통한 페이지 변경
         val mainBtnMain = findViewById<Button>(R.id.main_button_diaryList)
